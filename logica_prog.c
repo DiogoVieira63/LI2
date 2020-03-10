58dc1e201bf606132c389b29711daa0ff677cf7f
tomascardosofrancisco <62016068+tomascardosofrancisco@users.noreply.github.com>
2020-03-10 22:14:39 +0000

    Update logica_prog.c
    Alguns erros ainda para serem atualizados,mas a estrutura deve ser siimples de entender.

--- logica_prog.c
+++ logica_prog.c
@@ -1,4 +1,16 @@
-int jogar(ESTADO *e, COORDENADA c) {
-printf("jogar %d %d\n", c.coluna, c.linha);
-return 1;
+#include "logica.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+int jogar(ESTADO *e, COORDENADA c) {
+    int m,n;
+    m = e->ultima_jogada.coluna;
+    n = e->ultima_jogada.linha;
+    if (((abs (m - c.coluna) == 1) && (abs (n - c.linha) == 1)) || ((abs (m - c.coluna) == 0) && (abs (n - c.linha) == 1)) || ((abs (m - c.coluna) == 1) && (abs (n - c.linha) == 0))) {
+        if (c == VAZIO) { //Ainda falta corrigir o erro nesta linha.
+            printf("jogar %d %d\n", c.coluna, c.linha);
+            return 1;
+        }
+    }
+    return 0;
+}
