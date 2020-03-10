26d1d7286e05cf24b7561f9ff056d5489ba80930
JoseDiogoMartinsVieira <a93251@alunos.uminho.pt>
2020-03-10 20:10:33 +0000

    atualizado

--- c_dados.c
+++ c_dados.c
@@ -1,12 +1,31 @@
+void tab_inicial (CASA tab [8][8]){
+int linha = 1;
+while (linha <= 8){
+int coluna = 1;
+while (coluna <= 8) tab [linha] [coluna++] = VAZIO;
+linha++;
+}
+tab [5][4]= BRANCA; 
+}
+
+
 //Esta função deverá criar um estado vazio (com o tabuleiro inicializado)
 ESTADO *inicializar_estado() {
 ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
 e->jogador_atual = 1;
-e->num_jogadas = 0;
-
-// Falta a resto da inicialização.
+e->num_jogadas = 0; 
+/*
+e->jogadas->jogador1.linha = 0;
+e->jogadas->jogador1.coluna = 0;
+e->jogadas->jogador2.linha = 0;
+e->jogadas->jogador2.coluna = 0;
+*/
+tab_inicial (e->tab);
+e->ultima_jogada.linha = 0; // por ser a peça onde começa mas não sei se posso declarar assim
+e->ultima_jogada.coluna = 0;
 return e;
 }
+
 
 // Esta função permite obter o número do jogador atual
 int obter_jogador_atual(ESTADO *estado);
