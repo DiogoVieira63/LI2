5c50eab8dbfe7b90aad560c1191fbde5cb6a8ece
JoseDiogoMartinsVieira <a93251@alunos.uminho.pt>
2020-03-11 22:54:24 +0000

    atualizado

--- c_dados.h
+++ c_dados.h
@@ -1,5 +1,27 @@
 #ifndef ___DADOS_H___
 #define ___DADOS_H___
 
+
+typedef enum {VAZIO, BRANCA, PRETA} CASA;
+
+typedef struct {
+    int coluna;
+    int linha;
+    } COORDENADA;
+    
+typedef struct {
+    COORDENADA jogador1;
+    COORDENADA jogador2;
+    } JOGADA;
+    
+typedef JOGADA JOGADAS[32];
+
+typedef struct {
+    CASA tab[8][8];
+    JOGADAS jogadas;
+    COORDENADA ultima_jogada;
+    int num_jogadas;
+    int jogador_atual;
+    } ESTADO;
 ESTADO *inicializar_estado();
 #endif
