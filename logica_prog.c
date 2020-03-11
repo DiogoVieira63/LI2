c693da9bd951089d0099980e051eab3e1394b934
JoseDiogoMartinsVieira <a93251@alunos.uminho.pt>
2020-03-11 17:00:39 +0000

    update função jogar

--- logica_prog.c
+++ logica_prog.c
@@ -4,14 +4,14 @@
 
 int jogar(ESTADO *e, COORDENADA c) {
     int m,n;
-    m = e->ultima_jogada.coluna;
-    n = e->ultima_jogada.linha;
-    CASA peca.atual = e->tab [m][n]; 
+    m = e->ultima_jogada.coluna; //NÃO EXISTE
+    n = e->ultima_jogada.linha; // NÃO EXISTE
+    CASA peca_atual = e->tab [m][n]; 
     if ((abs (c.linha-n) <= 1) && (abs (c.coluna -m) <= 1) && !(c.linha == n && c.coluna == m)){
-            CASA peca.destino = e->tab [c.coluna][c.linha];
-            if (peca.destino == VAZIA) {
-                peca.destino = BRANCA;
-                peca.atual = PRETA;
+            CASA peca_destino = e->tab [c.coluna][c.linha];
+            if (peca_destino == VAZIO) {
+                peca_destino = BRANCA;
+                peca_atual = PRETA;
                 m = c.coluna;
                 n = c.linha;
             }
@@ -19,6 +19,5 @@ int jogar(ESTADO *e, COORDENADA c) {
             printf("jogar %d %d\n", c.coluna, c.linha);
             return 1;
         }
-    }
     return 0;
 }
