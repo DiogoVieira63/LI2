a24847c091a8b22b1628c267e5e45cf1649b7c96
JoseDiogoMartinsVieira <a93251@alunos.uminho.pt>
2020-03-11 22:41:37 +0000

    atualizado

--- logica_prog.c
+++ logica_prog.c
@@ -1,23 +1,43 @@
-#include "logica.h"
+#include "c_dados.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+ESTADO* alterar_estado_casa (ESTADO *e,COORDENADA c){
+int li = c.linha,col = c.coluna;
+if (e->tab [col][li] == VAZIO) e->tab [col][li] = BRANCA;
+else e->tab [col][li] = PRETA;
+return e;
+}
+
+ESTADO* alterar_num_jogadas (ESTADO *e){
+e->num_jogadas++;
+return e;
+}
+
+ESTADO* alterar_jogador_atual (ESTADO *e){
+if (e->jogador_atual == 1)e->jogador_atual++;
+else e->jogador_atual--;
+return e;
+}
+
 int jogar(ESTADO *e, COORDENADA c) {
     int m,n;
-    m = e->ultima_jogada.coluna; //NÃO EXISTE
-    n = e->ultima_jogada.linha; // NÃO EXISTE
+    m = e->ultima_jogada.coluna;
+    n = e->ultima_jogada.linha; 
     CASA peca_atual = e->tab [m][n]; 
     if ((abs (c.linha-n) <= 1) && (abs (c.coluna -m) <= 1) && !(c.linha == n && c.coluna == m)){
             CASA peca_destino = e->tab [c.coluna][c.linha];
             if (peca_destino == VAZIO) {
-                peca_destino = BRANCA;
-                peca_atual = PRETA;
-                m = c.coluna;
-                n = c.linha;
+            alterar_estado_casa (e,c);
+            alterar_estado_casa (e,e->ultima_jogada);
+            alterar_num_jogadas (e);
+            alterar_jogador_atual (e);
+            e->ultima_jogada =c;
             }
-            else printf ("Jogada Impoossível.");
+            else printf ("Jogada Impoossível. \n");
             printf("jogar %d %d\n", c.coluna, c.linha);
             return 1;
         }
+    else (printf ("Jogada Impossível \n"));
     return 0;
 }
