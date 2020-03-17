9a5665480b2c3e4ce7038155be3d62f90abdad58
JoseDiogoMartinsVieira <a93251@alunos.uminho.pt>
2020-03-17 11:32:05 +0000

    add comentários

--- c_interface.h
+++ c_interface.h
@@ -1,9 +1,30 @@
+/**
+@file c_interface.h
+Definição das funções que imrimem no ecrã
+*/
+
 #ifndef ___INTERFACE_H___
 #define ___INTERFACE_H___
 
-
+/**
+\brief Interpretador dos inputs do utilizador 
+@param e Apontador para o ESTADO
+@returns Um int caso seja executado devidamente ou não 
+*/
 int interpretador(ESTADO *e);
+/**
+\brief Imprime o tabuleiro
+@param e Apontador para o ESTADO
+*/
 void mostrar_tabuleiro(ESTADO *e);
+/**
+\brief Display da mensagem para o Vencedor
+@param e Apontador para o ESTADO
+*/
 void display_gameover (int n);
+/**
+\brief Display de mensagens de erro
+@param m Int para selecionar qual mensagem mostrar
+*/
 void print_erro (int n);
 #endif
--- logica_prog.h
+++ logica_prog.h
@@ -1,6 +1,11 @@
 #ifndef ___LOGICA_H___
 #define ___LOGICA_H___
 
+
+/**
+\brief Função que faz a jogada
+@param e Apontador para o ESTADO
+*/
 int jogar(ESTADO *e, COORDENADA c);
 int fim_do_jogo (ESTADO* e);
 #endif
